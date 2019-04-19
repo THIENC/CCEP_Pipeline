d2762221aab69174f4bcc83e0020473d3c41be91
ZZZHHH77 <794505917@qq.com>
2019-04-19 16:17:06 +0800

    Update loop.m

--- CCEP_Analysis_Pipeline/loop.m
+++ CCEP_Analysis_Pipeline/loop.m
@@ -2,13 +2,15 @@
 
 spm('defaults', 'EEG');
 edfFiles = dir('*.edf');
-for i = 1:1                %length(edfFiles)
+
+for i = 1:length(edfFiles)
     mkdir(edfFiles(i).name(1:end-4));
     movefile(edfFiles(i).name,edfFiles(i).name(1:end-4));
     cd(edfFiles(i).name(1:end-4));
+    
     %% File IO
     % load the raw edf data and convert it to SPM format
-    index = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,123,124,125];
+    index = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,123,124,125,126,127,128,129,130,131,132,133,134,135,136,137,138,139,140,141,142,143,144,145,146,147,148,149]
     D = edf_TO_SPM_converter_GUI(edfFiles(i).name,index,'meeg_');
 
     % load and convert the DC channel
@@ -101,17 +103,19 @@ for i = 1:1                %length(edfFiles)
     close;
     %% plot 
     for i = 1:length(D.chanlabels) 
-       ChannelInd = i;
-       Data = squeeze(D(ChannelInd,:,:));
-       figure
-       plot(Data)
-       %str1 = char(D.chanlabels(i))
-       title(D.chanlabels(i));
-       %set(gca,'FontSize',20)
-       %set(findobj(gca,'type','line'),'linew',4)
-       set(gcf,'Position',[0 100 1920 600])
-       print(D.chanlabels{i},'-dpng')
-       close
+        ChannelInd = i;
+        Data = squeeze(D(ChannelInd,:,:));
+        figure
+        plot(D.time,Data,'Color',[0.5 0.5 0.5]);
+        grid on
+        hold on
+        plot(D.time,mean(Data,2),'LineWidth',2,'Color','r')
+        %str1 = char(D.chanlabels(i))
+        title(D.chanlabels(i));
+        set(gca,'FontSize',14)
+        %set(findobj(gca,'type','line'),'linew',4)
+        print(D.chanlabels{i},'-dpng')
+        close
    end
    cd ..
 end
