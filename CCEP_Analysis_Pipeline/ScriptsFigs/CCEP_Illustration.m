8665dee8da522a54922f3df10d8707d9e959a8f2
zhaobaotian <zhaobaotian0220@foxmail.com>
2019-11-21 12:20:38 +0800

    update

--- CCEP_Analysis_Pipeline/ScriptsFigs/CCEP_Illustration.m
+++ CCEP_Analysis_Pipeline/ScriptsFigs/CCEP_Illustration.m
@@ -60,11 +60,52 @@ Sw2 = ones([1000 1]) + 1;
 Sw3 = ones([1000 1]) - 1;
 Sw4 = ones([2000 1]);
 
-plot([Sw1;Sw2;Sw3;Sw4],'LineWidth',2)
-axis off
-set(gcf,'Position',[0 100 300 500])
+plot([Sw1;Sw2;Sw3;Sw4],'LineWidth',2,'Color',cdcol.lightblue)
+ylabel('Amplitude')
+xlabel('Time (¦Ìs)')
+set(gcf,'Position',[0 100 600 600])
 print(['SquareWaveStim'],'-r600','-dpng')
 close
+
+% Evoked potentials
+cd('I:\CCEP_fig\CCEP_Raw_060\l5l6')
+
+addpath('D:\THIENC_iEEG_Task_Preprocessing_Base\Externals\plotAreaErrorbar')
+addpath('D:\spm12_7219')
+spm('defaults','eeg')
+D = spm_eeg_load();
+EvokeChan = 77;
+AllChan = [1:40];
+BadChan = [40 38 32 29 26 20 17 13 6 5 1];
+GoodChans = setdiff(AllChan,BadChan)
+DataPlot = squeeze(D(EvokeChan,300:1000,GoodChans))';
+h = figure(1)
+rectangle('Position',[215 -70 285 180],'Curvature',0.2,'FaceColor',[0.5 0.5 0.5 0.2],'EdgeColor','none')
+hold on
+options.handle     = h;
+options.color_area = [128 193 219]./255;    % Blue theme
+options.color_line = [ 52 148 186]./255;
+%options.color_area = [243 169 114]./255;    % Orange theme
+%options.color_line = [236 112  22]./255;
+options.alpha      = 0.5;
+options.line_width = 3;
+options.error      = 'sem';
+box on
+plot_areaerrorbar(DataPlot,options)
+axis tight
+set(gcf,'Position',[0 100 800 500])
+print(['PT060_Sl56_Rf34'],'-r600','-dpng')
+close
+
+
+% figure
+% for i = 1:40
+%     figure
+%     plot(squeeze(D(EvokeChan,300:1000,i)))
+%     title(i)
+% end
+
+
 
 
 
